7bd0b79e277f4a97fa9b06e9ed958d2034b2ec0a
Rodrigo Sánchez <30.rodrigo.12@gmail.com>
2026-01-26 09:46:44 +0100

    update logMessage function to delete the first messages when it exceeds a maximum number of messages

--- assets/js/game.js
+++ assets/js/game.js
@@ -511,13 +511,20 @@ function deleteInput() {
 
 function logMessage(text) {
   let historyList = document.getElementById("historyList");
+  const maxMessages = 50; // variable para establecer el maximo de mensajes posibles al mismo tiempo
 
+  // crea un elemento nuevo y se le inserta la variabel text como contenido con innerHTML
   const newLi = document.createElement("li");
   newLi.innerHTML = text;
-  historyList.appendChild(newLi);
+  historyList.appendChild(newLi); // se añade el elemento creado al historial
 
   // asegura que siempre se vea el ultimo mensaje
   historyList.scrollTop = historyList.scrollHeight;
+
+  // si la cantidad de mensajes (child) en historyList supera el maximo (maxMessages) borra el primero
+  if (historyList.childElementCount > maxMessages) {
+    historyList.removeChild(historyList.firstChild);
+  }
 }
 
 // funcion para actualizar los datos que se muestran en pantalla y la logica encargada de generar enemigos
